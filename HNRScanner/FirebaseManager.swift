0f3d7239b359123922020fb7cd72b8f25efa540d
Deshun Cai <unusep@gmail.com>
2018-05-11 18:29:12 +0800

    add firebase implementation

--- HNRScanner/FirebaseManager.swift
+++ HNRScanner/FirebaseManager.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import Firebase
+
+enum RegistrationOutcome {
+    case SuccessfullyRegistered
+    case AlreadyRegistered
+    case UserDoesNotExist
+}
+
+
+class FirebaseManager {
+    
+    static let sharedInstance = FirebaseManager()
+    let db = Firestore.firestore()
+    
+    private init(){}
+    
+    static func getInstance() -> FirebaseManager {
+        return sharedInstance
+    }
+    
+    func register(user : String, completion: @escaping (RegistrationOutcome) -> ()) {
+        let docRef = db.collection("users").document(user)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let hasPhysicallyRegistered : Bool = document.data()?["physicallyRegistered"] as? Bool ?? false
+                if hasPhysicallyRegistered {
+                    completion(RegistrationOutcome.AlreadyRegistered)
+                } else {
+                    docRef.updateData(["physicallyRegistered" : true]) { err in
+                        if let err = err {
+                            print("Error updating document: \(err)")
+                        } else {
+                            completion(RegistrationOutcome.SuccessfullyRegistered)
+                        }
+                    }
+                }
+                
+            } else {
+                completion(RegistrationOutcome.UserDoesNotExist)
+            }
+        }
+    }
+}
