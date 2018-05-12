5ab4169d1b7e11fb2c329f6514896203c82215d7
Suyash Shekhar <suyashshekhar@gmail.com>
2018-05-12 12:35:04 +0800

    Add Subclassed Scanner

---
+++ HNRScanner/ScannerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ScannerViewController.swift
+//  HNRScanner
+//
+//  Created by Suyash Shekhar on 12/5/18.
+//  Copyright © 2018 NUS Hackers. All rights reserved.
+//
+
+import UIKit
+import BarcodeScanner
+
+class ScannerViewController: BarcodeScannerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
